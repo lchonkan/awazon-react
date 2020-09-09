0cea818432f8472be3869e5378aa7378bd0c6ed5
Leonardo Chonkan <lchonkan@forgebim.com>
2020-09-09 03:20:46 -0600

    added the subtotal component to the checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Checkout.css';
+import Subtotal from './Subtotal';
 
 function Checkout() {
     return (
@@ -20,7 +21,7 @@ function Checkout() {
             </div>
 
             <div className='checkout__right'>
-                <h2>The sub total will go here</h2>
+                <Subtotal />
             </div>
         </div>
     );
