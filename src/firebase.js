9564d93bf80ace0f52066c64a7fcb75e01a98513
Leonardo Chonkan <lchonkan@forgebim.com>
2020-09-08 21:23:43 -0600

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyA3i8dpSOgjK0hxTlw1B63rUaNirdLrcMM',
+    authDomain: 'clo-15d92.firebaseapp.com',
+    databaseURL: 'https://clo-15d92.firebaseio.com',
+    projectId: 'clo-15d92',
+    storageBucket: 'clo-15d92.appspot.com',
+    messagingSenderId: '456916462988',
+    appId: '1:456916462988:web:460e2b1036de0dc32fbaa9',
+};
