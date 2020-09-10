920df6878e10c0c6a3c313f73ebf41011cb7ff24
Leonardo Chonkan <lchonkan@forgebim.com>
2020-09-10 01:10:42 -0600

    set up firebase config and initialized it in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: 'AIzaSyA3i8dpSOgjK0hxTlw1B63rUaNirdLrcMM',
     authDomain: 'clo-15d92.firebaseapp.com',
@@ -7,3 +9,5 @@ const firebaseConfig = {
     messagingSenderId: '456916462988',
     appId: '1:456916462988:web:460e2b1036de0dc32fbaa9',
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
