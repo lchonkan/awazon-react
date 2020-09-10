1a733e91f3ce0e20d0b590fc801f206b83cfceca
Leonardo Chonkan <lchonkan@forgebim.com>
2020-09-10 01:02:27 -0600

    hooked up the buttons to placeholder events

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { Link } from 'react-router-dom';
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = (e) => {
+        e.preventDefault();
+        //Todo some fancy firebase shiiiiiiiiiiiiiiit
+    };
+
+    const register = (e) => {
+        e.preventDefault();
+        //Todo resgister logic (firebase)
+    };
+
     return (
         <div className='login'>
             <Link to='/'>
@@ -18,13 +31,23 @@ function Login() {
                     <h1>Sign-in</h1>
                     <form action=''>
                         <h5>Email (phone for mobile accounts)</h5>
-                        <input type='text' />
+                        <input
+                            type='text'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
                         <h5>Password</h5>
-                        <input type='password' />
-                        <button className='login__signInButton'>Continue</button>
+                        <input
+                            type='password'
+                            value={password}
+                            onChange={(e) => setEmail(e.target.password)}
+                        />
+                        <button onClick={signIn} className='login__signInButton'>
+                            Continue
+                        </button>
                     </form>
                     <p>
-                        By continuing, you agree to Awazon FAKE CLONE Conditions of Use and Privacy
+                        By continuing, you agree to Awazon's Fake Conditions of Use and Privacy
                         Notice.
                     </p>
                 </div>
@@ -34,7 +57,9 @@ function Login() {
                         <h5>New to Awazon?</h5>
                     </div>
 
-                    <button className='login__registerButton'>Create your Awazon account</button>
+                    <button onClick={register} className='login__registerButton'>
+                        Create your Awazon account
+                    </button>
                 </div>
             </div>
         </div>
