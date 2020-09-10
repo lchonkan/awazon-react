a402937e909f43a302e5e54482678124b4c97fe2
Leonardo Chonkan <lchonkan@forgebim.com>
2020-09-10 02:14:03 -0600

    implemented use effect on app.js to get the current user if logged in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
 import Login from './Login';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
 
 function App() {
+    // eslint-disable-next-line no-empty-pattern
+    const [{}, dispatch] = useStateValue();
+
+    //will only run once when the app component loads
+    useEffect(() => {
+        auth.onAuthStateChanged((authUser) => {
+            console.log('The user is >>>', authUser);
+            if (authUser) {
+                //the user just logged in / the user was logged in
+                dispatch({
+                    type: 'SET_USER',
+                    user: authUser,
+                });
+            } else {
+                //the user is logged out
+                dispatch({
+                    type: 'SET_USER',
+                    user: null,
+                });
+            }
+        });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
         // BEM
         <Router>
