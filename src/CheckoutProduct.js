0752352f34e2219657c21c133fa0fadb537c971f
Leonardo Chonkan <lchonkan@forgebim.com>
2020-09-09 23:16:06 -0600

    added functionality to remove the products from the basket in the reducer.js class

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './CheckoutProduct.css';
 import StarIcon from '@material-ui/icons/Star';
+import { useStateValue } from './StateProvider';
 
 function CheckoutProduct({ id, image, title, price, rating }) {
     // This piece of code is to break down the price for proper styling
     const priceComponents = price.toString().split('.');
     const dollars = priceComponents[0];
     const cents = priceComponents[1];
+
+    // eslint-disable-next-line no-unused-vars
+    const [{ basket }, dispatch] = useStateValue();
+
+    const removeFromBasket = () => {
+        //remove the item from the Basket
+        dispatch({ type: 'REMOVE_FROM_BASKET', id: id });
+    };
 
     return (
         <div className='checkoutProduct'>
@@ -25,7 +34,7 @@ function CheckoutProduct({ id, image, title, price, rating }) {
                             <StarIcon className='star'></StarIcon>
                         ))}
                 </div>
-                <button>Remove from Basket</button>
+                <button onClick={removeFromBasket}>Remove from Basket</button>
             </div>
         </div>
     );
--- src/reducer.js
+++ src/reducer.js
@@ -10,6 +10,21 @@ const reducer = (state, action) => {
                 ...state,
                 basket: [...state.basket, action.item],
             };
+
+        case 'REMOVE_FROM_BASKET':
+            const index = state.basket.findIndex((basketItem) => basketItem.id === action.id);
+            let newBasket = [...state.basket];
+            if (index >= 0) {
+                newBasket.splice(index, 1);
+            } else {
+                console.warn(
+                    `Cant remove the product (id: ${action.id}) as it is NOT in the basket`
+                );
+            }
+            return {
+                ...state,
+                basket: newBasket,
+            };
         default:
             return state;
     }
