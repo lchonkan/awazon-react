67614c6d1be2ad518c7a14967db0afd04f6cf519
Leonardo Chonkan <lchonkan@forgebim.com>
2020-09-10 00:51:49 -0600

    styles the login page

--- src/Login.js
+++ src/Login.js
@@ -4,14 +4,40 @@ import { Link } from 'react-router-dom';
 
 function Login() {
     return (
-        <Link to='/'>
-            <div className='login'>
+        <div className='login'>
+            <Link to='/'>
                 <img
-                    src='https://turnerduckworth.com/sites/default/files/styles/case_study_single_image/public/2019-03/AZN_logo.jpg?h=a92f03cd&itok=AjcRaJtK'
+                    className='login__logo'
+                    src='https://1000logos.net/wp-content/uploads/2016/10/Amazon-Logo-500x200.png'
                     alt=''
                 />
+            </Link>
+
+            <div className='top_container'>
+                <div className='login__container'>
+                    <h1>Sign-in</h1>
+                    <form action=''>
+                        <h5>Email (phone for mobile accounts)</h5>
+                        <input type='text' />
+                        <h5>Password</h5>
+                        <input type='password' />
+                        <button className='login__signInButton'>Continue</button>
+                    </form>
+                    <p>
+                        By continuing, you agree to Awazon FAKE CLONE Conditions of Use and Privacy
+                        Notice.
+                    </p>
+                </div>
+
+                <div className='login__containerBreak'>
+                    <div className='login__containerBreakLine'>
+                        <h5>New to Awazon?</h5>
+                    </div>
+
+                    <button className='login__registerButton'>Create your Awazon account</button>
+                </div>
             </div>
-        </Link>
+        </div>
     );
 }
 
